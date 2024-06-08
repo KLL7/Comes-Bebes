c6c1e021bd3f2c29f093f70fc2a566ed5399f148
Isaac Sales Rodrigues <124395470+IsaacSales15@users.noreply.github.com>
2024-06-08 19:57:21 -0300

    Stay With Me
    Atualizações:
    - Modificações no "Login System".
       ° Correções de erros no Pop-Up.
       °  Correções de erros no Login.
       ° Correções de erros no Register.

--- scripts/login.js
+++ scripts/login.js
@@ -1,34 +1,34 @@
-document.getElementById("submit-btn").addEventListener("click", function () {
-  var newUser = document.getElementById("register-email").value;
-  var newPassword = document.getElementById("register-password").value;
-
-  var users = JSON.parse(localStorage.getItem("users")) || [];
-  users.push({ username: newUser, password: newPassword });
-  localStorage.setItem("users", JSON.stringify(users));
-
-  alert("Cadastro realizado com sucesso!");
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("login-btn")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+  // Função de cadastro
+  document.getElementById("submit-btn").addEventListener("click", function () {
+    var newUser = document.getElementById("register-email").value;
+    var newPassword = document.getElementById("register-password").value;
 
-      var username = document.getElementById("user-email").value;
-      var password = document.getElementById("user-password").value;
+    var users = JSON.parse(localStorage.getItem("users")) || [];
+    users.push({ username: newUser, password: newPassword });
+    localStorage.setItem("users", JSON.stringify(users));
 
-      var users = JSON.parse(localStorage.getItem("users")) || [];
+    alert("Cadastro realizado com sucesso!");
+  });
 
-      var foundUser = users.find(function (user) {
-        return user.username === username && user.password === password;
-      });
+  // Função de login
+  document.getElementById("login-form").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      if (foundUser) {
-        alert("Login bem-sucedido!");
-        window.location.href = "../index.html";
-      } else {
-        alert("Usuário ou senha inválidos. Por favor, cadastre-se primeiro.");
-      }
-    });
+    var username = document.getElementById("user-email").value;
+    var password = document.getElementById("user-password").value;
+
+    var users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const foundUser = users.find(
+      user => user.username === username && user.password === password
+    );
+
+    if (foundUser) {
+      alert("Login bem-sucedido!");
+      window.location.href = "../index.html";
+    } else {
+      alert("Usuário ou senha inválidos. Por favor, cadastre-se primeiro.");
+    }
+  });
 });
--- scripts/pop.js
+++ scripts/pop.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const registerBtn = document.getElementById("register-btn");
-  const pop = document.getElementById("pop");
+  const pop = document.getElementById("register");
   const closeBtn = document.querySelector(".close-btn");
 
-  registerBtn.addEventListener("click", function () {
-    pop.style.display = "block";
+// Função para mostrar o Pop-Uo
+  registerBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    pop.classList.remove("hidden");
   });
 
+// Função para fechar o Pop-Up
   closeBtn.addEventListener("click", function () {
-    pop.style.display = "none";
+    pop.classList.add("hidden");
   });
 });
-
