145396c46d609071bb2babbfdab44ff64d20a992
Kayo Lucas Lima <kayodomine@gmail.com>
2024-06-11 16:33:42 -0300

    Salamaleico
    Halakba!

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,43 +1,44 @@
-//base do código é o pomodoro    
+//base do código é o pomodoro
+var painelCart = document.querySelector("#cart-item-container");    
+//delete cardincart
+
 const cards = document.querySelectorAll(".item-card")
 cards.forEach((card) => {
-const nameV = card.querySelector(".item-name");
-const priceV = card.querySelector(".item-price");
-const quantityV = card.querySelector(".item-quantity");
-const imgV = card.querySelector(".item-img");
-
-const name = nameV.textContent;
-const price = priceV.textContent;
-const quantity = quantityV.textContent;
-const img = imgV.src;
+const nameV = card.querySelector(".item-name").innerText;
+const priceV = card.querySelector(".item-price").innerText;
+const quantityV = card.querySelector(".item-quantity").value;
+const imgV = card.querySelector(".item-img").src;
 
 card.addEventListener("click", () =>{
     if (quantityV <= 0){
         alert("Você tem que pedir algum para adicionar ao carrinho");
+        return
     }
-    console.log(nameV, priceV, quantityV, imgV);
-    addcardtocart(name, price, quantity, img);
+    
+    addcardtocart()
+
+    function addcardtocart() {
+        var oldCart = painelCart.innerHTML;
+    
+        var newCart = painelCart.innerHTML = `
+        <div class="cart-item">
+            <div>
+                <span class="item-name">${nameV}</span>
+                <div>
+                    <img id="delete" src="images/delet-delete-cut-svgrepo-com.svg" alt="plus" />
+                    <span class="item-quantity">${quantityV}</span>
+                    <span class="item-price">${priceV}</span>
+                </div>
+            </div>
+            <img class="item-img" src="${imgV}" alt="pastel" />
+        </div>
+        `
+    
+        oldCart = newCart + oldCart
+        painelCart.innerHTML = oldCart;
+    }
 }
 )
 })
 
 
-function addcardtocart(name, price, quantity, img){
-    const painelCart = document.querySelector(".cart-item-container");
-    const oldCart = painelCart.innerHTML;
-
-    painelCart.innerHTML = `
-    <div class="cart-item">
-        <div>
-            <span>${name}</span>
-            <div>
-                <span>${quantity}</span>
-                <span>${price}</span>
-            </div>
-        </div>
-        <img class="item-img" src="${img}" alt="pastel" />
-    </div>
-    `
-
-    painelCart.innerHTML = oldCart;
-}
