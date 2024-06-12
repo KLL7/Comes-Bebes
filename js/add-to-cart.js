5c96e48c4719c3ed02a40f8247530fec025fe952
Kayo Lucas Lima <kayodomine@gmail.com>
2024-06-12 09:08:27 -0300

    X-cannabis
    carrinho tá feito, agora é finalizar compra

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,8 +1,7 @@
 //base do código é o pomodoro
 var painelCart = document.querySelector("#cart-item-container");
-var priceTotal = document.querySelector("#cart-total").innerText;
-//delete
-//delete cardincart
+var pricePainel = document.querySelector(".cart-total-value").innerText;
+var buttonFinish = document.querySelector("#finish-button");
 
 const cards = document.querySelectorAll(".item-card")
 cards.forEach((card) => {
@@ -16,30 +15,46 @@ card.addEventListener("click", () =>{
         alert("Você tem que pedir algum para adicionar ao carrinho");
         return
     }
-
+    
     var newcard = document.createElement("div");
     newcard.classList.add("cart-item");
     newcard.innerHTML = `
     <div>
-        <span class="item-name">${nameV}</span>
-        <div>
-            <img id="delete" src="images/delet-delete-cut-svgrepo-com.svg" alt="plus" />
+    <span class="item-name">${nameV}</span>
+    <div>
+    <img id="delete" src="images/delet-delete-cut-svgrepo-com.svg" alt="plus" />
             <span class="item-quantity">${quantityV}</span>
             <span class="item-price">${priceV}</span>
-        </div>
+            </div>
     </div>
     <img class="item-img" src="${imgV}" alt="hamburguer" />
     `;
+    
 
+    var price = parseFloat(pricePainel);
+    const priceValue = priceV.replace('R$', '');
+    price = price + parseFloat(priceValue);
+    console.log(price+"price");
+    pricePainel = price
+    document.querySelector(".cart-total-value").innerText = "R$ "+price;
 
     const del = newcard.querySelector("#delete");
     del.addEventListener("click", () => {
+        var delprice = parseFloat(pricePainel);
+        const delpriceValue = priceV.replace('R$', '');
+        delprice = delprice - parseFloat(delpriceValue);
+        console.log(delprice+"delprice");
+        pricePainel = delprice
+        document.querySelector(".cart-total-value").innerText = "R$ "+delprice;
         newcard.remove();
     })
 
-
+    
     painelCart.append(newcard);
-
+    
     }
 )
+})
+
+buttonFinish.addEventListener("click", () => {
 })
